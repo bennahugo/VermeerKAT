46cdb12566125c1f0f201e197f1b8c7ed1aa97b1
bennahugo <bennahugo@aol.com>
2020-01-02 23:49:31 +0200

    remove unused form

--- vermeerkat/plugins/BAT/viewcontrollers/__init__.py
+++ vermeerkat/plugins/BAT/viewcontrollers/__init__.py
@@ -2,7 +2,6 @@ import npyscreen
 
 from vermeerkat.plugins.BAT.viewcontrollers.entry_form import entry_form
 from vermeerkat.plugins.BAT.viewcontrollers.option_editor import options_form
-from vermeerkat.plugins.BAT.viewcontrollers.execution_form import execution_form
 class event_loop(npyscreen.NPSAppManaged):
     STEPS = None
 
@@ -12,4 +11,3 @@ class event_loop(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm("MAIN", entry_form, name="Welcome")
         self.addForm("OPTIONVIEW", options_form, name="BAT options")
-        self.addForm("EXECUTIONVIEW", execution_form, name="Executing BAT")
